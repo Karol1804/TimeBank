1f82a91d908fbeb0ef97e087e3395df8a8753f17
vadmik00 <mikulas.vadoc@t-systems.com>
2022-04-29 15:23:02 +0200

    added estimated to helper function,localhostApi

--- timebank/src/app/services/services.service.ts
+++ timebank/src/app/services/services.service.ts
@@ -12,7 +12,7 @@ import { map } from 'rxjs';
 
 export class ServicesService {
 
-  private api = "http://157.245.27.101:5000/api/v1/";  //=======#testing environment
+  private api = "http://localhost:5000/api/v1/";  //=======#testing environment
   // private api = "http://157.230.79.85:5000/api/v1/";  //=======#production environment
   // private api = "http://localhost:5000/api/v1/";  //=======#local
   private apiGetServicesUrl = this.api + "services";
@@ -38,7 +38,7 @@ export class ServicesService {
   remoteServices(res: any): Service[] {
     let services: Service[] = [];
     for (let service of res) {
-      services.push(new Service(service.id, service.title, service.User.user_name, service.User.phone, service.User.id))
+      services.push(new Service(service.id, service.title, service.User.user_name, service.estimated, service.User.phone, service.User.id))
     }
     return services;
   }
