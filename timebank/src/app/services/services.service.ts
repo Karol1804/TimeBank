c6abf413cd8d40189457048ca3749954b530de8e
Milan Juhas <thejustinian111@gmail.com>
2022-04-30 18:07:14 +0200

    Editing .gitlab-ci.yml

--- timebank/src/app/services/services.service.ts
+++ timebank/src/app/services/services.service.ts
@@ -12,14 +12,15 @@ import { map } from 'rxjs';
 
 export class ServicesService {
 
-  if (configuration=production)
-  {
-    private api = "http://157.230.79.85:5000/api/v1/";
-  }
-  else
-  {
-    private api = "http://157.245.27.101:5000/api/v1/";  //=======#testing environment
-  }
+//   if (configuration=production)
+//   {
+//     private api = "http://157.230.79.85:5000/api/v1/";
+//   }
+//   else
+//   {
+//     private api = "http://157.245.27.101:5000/api/v1/";  //=======#testing environment
+//   }
+  private api = "http://157.245.27.101:5000/api/v1/";
   // private api = "http://157.230.79.85:5000/api/v1/";  //=======#production environment
   // private api = "http://localhost:5000/api/v1/";  //=======#local
   private apiGetServicesUrl = this.api + "services";
