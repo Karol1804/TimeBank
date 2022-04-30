62ffbb54c8dcbad90a095f90f7968430b6385d7f
Milan Juhas <thejustinian111@gmail.com>
2022-04-30 20:07:39 +0200

    Editing services.service.ts

--- timebank/src/app/services/services.service.ts
+++ timebank/src/app/services/services.service.ts
@@ -5,8 +5,8 @@ import { User } from '../models/user';
 import { EndRegisterRecord, GetRegisterRecord, RegisterRecord } from '../models/registerrecord';
 import { map } from 'rxjs';
 
-var url = fetch('./url.txt')
-console.log(url)
+// var url = fetch('./url.txt')
+// console.log(url)
 //   .then(response => response.text())
 //   .then(window.data => {
 //   	// Do something with your data
@@ -29,8 +29,8 @@ export class ServicesService {
 //   }
 //   const fs = require('fs');
 
-  private api = url;
-  // private api = "http://157.245.27.101:5000/api/v1/";  //=======#testing environment
+//   private api = url;
+  private api = "http://157.245.27.101:5000/api/v1/";  //=======#testing environment
   // private api = "http://157.230.79.85:5000/api/v1/";  //=======#production environment
   // private api = "http://localhost:5000/api/v1/";  //=======#local
   private apiGetServicesUrl = this.api + "services";
