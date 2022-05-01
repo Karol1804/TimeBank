d6115bdd9da0aef999aba4178856fab56ae8cd1f
juhmil00 <juhmil00@example.com>
2022-05-01 06:02:55 +0000

    Update services.service.ts

--- timebank/src/app/services/services.service.ts
+++ timebank/src/app/services/services.service.ts
@@ -6,7 +6,7 @@ import { EndRegisterRecord, GetRegisterRecord, RegisterRecord } from '../models/
 import { map } from 'rxjs';
 import { readFileSync } from 'fs';
 
-// const url = readFileSync('./url.txt', 'utf8');
+const url = readFileSync('./timebank/src/app/services/url.txt', 'utf8');
 // var url = fetch('./url.txt')
 // console.log(url)
 //   .then(response => response.text())
@@ -31,8 +31,8 @@ export class ServicesService {
 //   }
 //   const fs = require('fs');
 
-  // private api = url;
-   private api = "http://157.245.27.101:5000/api/v1/";  //=======#testing environment
+private api = url;
+// private api = "http://157.245.27.101:5000/api/v1/";  //=======#testing environment
   // private api = "http://157.230.79.85:5000/api/v1/";  //=======#production environment
   // private api = "http://localhost:5000/api/v1/";  //=======#local
   private apiGetServicesUrl = this.api + "services";
