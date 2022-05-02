6d7940bdfc781ec577f4232544d9898121bf3f13
harmar00 <mharvanek@t-systems.com>
2022-05-02 16:07:31 +0200

    added logged user name to Navbar

--- timebank/src/app/nav/nav.component.ts
+++ timebank/src/app/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { GlobalStorageService } from '../services/global-storage.service';
+import { ServicesService } from '../services/services.service';
 
 @Component({
   selector: 'app-nav',
@@ -11,13 +12,19 @@ export class NavComponent implements OnInit {
   GlobalStorageService: any;
   userId: any;
   userName: any;
+  user_name: string;
+  phone: string;
+  id: number;
 
-  constructor( private globalStorage: GlobalStorageService
+  constructor( 
+    private globalStorage: GlobalStorageService,
+    private servicesService: ServicesService
     ) { }
 
   ngOnInit(): void {
     let userId = this.globalStorage.getUserId();
     let userName = this.globalStorage.getUserName();
+    this.loadUser(userId);
     //this.loadId()
     console.log("userId je " + userId);
     console.log("userName je " + userName);
@@ -29,7 +36,15 @@ export class NavComponent implements OnInit {
     return !!this.globalStorage.getToken()
   }
 
-   
+  loadUser(userId: any) {
+    this.servicesService.getUser(userId).subscribe(user => {
+      this.user_name = user[0].user_name;
+      this.phone = user[0].phone;
+      this.id = user[0].id;
+      console.log("Toto user:")
+      console.log(user)
+    })
+  }
   
 }
 
