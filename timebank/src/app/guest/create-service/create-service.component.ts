2e880b6ea8ba0c5b01663743fc4a7d481ad40309
Robert Novotny <novotnr0@gmail.com>
2022-05-03 22:50:52 +0200

    Revert "create service update"
    This reverts commit 223db6ffb9e52eb40d25a560c45f7cc0722dae77.

--- timebank/src/app/guest/create-service/create-service.component.ts
+++ timebank/src/app/guest/create-service/create-service.component.ts
@@ -1,88 +1,58 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { Observable, Observer } from 'rxjs';
-import { GlobalStorageService } from 'src/app/services/global-storage.service';
+import { Router } from '@angular/router';
 import { Service } from '../../models/service';
 import { User } from '../../models/user';
 import { ServicesService } from '../../services/services.service';
+
 
 @Component({
   selector: 'app-create-service',
   templateUrl: './create-service.component.html',
   styleUrls: ['./create-service.component.css'],
 })
+
 export class CreateServiceComponent implements OnInit {
-  // for create new Service
+
+  public users: User[];
   public id: number;
   public title: string;
-  public user: string;
-  public estimate: number;
-  public avg_rating: number;
+  public user:string;
+  public estimate:number;
+  public avg_rating:number;
   public phone: string;
   public user_id: number;
   public emptyInputError = false;
-  // load logged username, phone, id
-  public obj_user_name: string;
-  public obj_phone: string;
-  public obj_id: number;
 
   constructor(
-    private globalStorage: GlobalStorageService,
-    private servicesService: ServicesService,
-    private router:Router
-  ) {}
+    private serviceService: ServicesService,
+    private router: Router,
+    ) {}
 
   ngOnInit(): void {
-    let userId = this.globalStorage.getUserId();
-    this.loadUser(userId);
+    this.loadUser();
   }
 
-  loadUser(userId: any) {
-    this.servicesService.getUser(userId).subscribe((user) => {
-      this.obj_user_name = user[0].user_name;
-      this.obj_phone = user[0].phone;
-      this.obj_id = user[0].id;
-    });
+  loadUser(){
+    this.serviceService.getUsers("").subscribe(remoteUsers => {
+      this.users = remoteUsers
+      console.log(this.users)
+    })
   }
 
-  // method to create new service
   addNewOffer() {
     this.emptyInputError = false;
-    if (!this.title || !this.estimate) {
+    if (!this.user_id || !this.title) {
       this.emptyInputError = true;
     } else {
- 
-    let service = new Service(
-      this.id,
-      this.title,
-      this.user,
-      this.estimate,
-      this.avg_rating,
-      this.phone,
-      (this.user_id = this.obj_id)
-    );
-    console.log('pridavam novy service');
-    console.log(service);
-
-    this.servicesService.saveCreatedService(service).subscribe((result) => {
-      console.log('new service added');
+      let service = new Service(this.id, this.title, this.user, this.estimate, this.avg_rating, this.phone, this.user_id,);
+      console.log('pridavam novy service');
       console.log(service);
-    });
-    this.router.navigate(['/'])
-  }
-}
 
-
-  // method for control input
-  omit_special_char(event: { charCode: number }) {
-    var k: number;
-    k = event.charCode; //         k = event.keyCode;  (Both can be used)
-    return (
-      (k > 64 && k < 91) ||
-      (k > 96 && k < 123) ||
-      k == 8 ||
-      k == 32 ||
-      (k >= 48 && k <= 57)
-    );
+      this.serviceService.saveCreatedService(service).subscribe((result) => {
+        console.log('new service added');
+        console.log(service);
+      });
+      this.router.navigate(['/'])
+    }
   }
 }
