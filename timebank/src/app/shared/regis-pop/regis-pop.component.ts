50b021498deffef00d36444878e248b87e6f1c54
Robert K <roborobo9a@gmail.com>
2022-05-07 21:48:14 +0200

    registracia v.08

--- timebank/src/app/shared/regis-pop/regis-pop.component.ts
+++ timebank/src/app/shared/regis-pop/regis-pop.component.ts
@@ -28,7 +28,7 @@ export class RegisPopComponent implements OnInit, OnDestroy {
   phoneSendResp: { result: boolean };
   private phoneJoin: string;
   formGroup: FormGroup;
-  titleAlert: string = 'This field is required';
+  //titleAlert: string = 'This field is required';
   post: any = '';
   user_registred: RegisUserResp;
 
@@ -57,7 +57,7 @@ export class RegisPopComponent implements OnInit, OnDestroy {
     this.formGroup = new FormGroup(
       {
         name: new FormControl(null, {
-          validators: Validators.required,
+          validators:[Validators.required, Validators.maxLength(15), Validators.minLength(3)]
         }),
 
         myphone: new FormControl(new MyTel('', '', '', ''), {
@@ -66,10 +66,10 @@ export class RegisPopComponent implements OnInit, OnDestroy {
         }),
 
         password: new FormControl(null, {
-          validators: Validators.required,
+          validators:[Validators.required, Validators.maxLength(20), Validators.minLength(5)]
         }),
         password_valid: new FormControl(null, {
-          validators: Validators.required,
+          validators: [Validators.required, Validators.maxLength(20), Validators.minLength(5)]
         }),
       },
       ValidService.match('password', 'password_valid')
@@ -84,21 +84,53 @@ export class RegisPopComponent implements OnInit, OnDestroy {
     return this.formGroup.get('myphone') as FormControl;
   }
 
+
+
+  getErrorName() {
+    return this.formGroup.get('name')?.hasError('required')
+      ? 'The field is required'
+      : this.formGroup.get('name')?.hasError('minlength')
+      ? 'Name should be min.3 characters long ' 
+      : this.formGroup.get('name')?.hasError('maxlength')
+      ? 'Name should be max.15 characters long '
+      : '';
+  }
+
+
+
+
   getErrorPhone() {
     return this.formGroup.get('myphone')?.hasError('required')
       ? 'The field is required'
       : this.formGroup.get('myphone')?.hasError('alreadyTaken')
-      ? 'This phone is already in use'
+      ? 'This phone is already in use !'
       : '';
   }
 
   getErrorPassword() {
     return this.formGroup.get('password')?.hasError('required')
       ? 'The field is required'
+      : this.formGroup.get('password')?.hasError('matching')
+      ? 'Passwords are NOT identical. '  
+      : this.formGroup.get('password')?.hasError('minlength')
+      ? 'Password should be min.5 characters long ' 
+      : this.formGroup.get('password')?.hasError('maxlength')
+      ? 'Password should be max.20 characters long ' :
+       '';
+  }
+
+  getErrorPassword_val() {
+    return this.formGroup.get('password_valid')?.hasError('required')
+      ? 'The field is required'
       : this.formGroup.get('password_valid')?.hasError('matching')
-      ? 'Passwords are NOT identical. '
+      ? 'Passwords are NOT identical. '  
+      : this.formGroup.get('password_valid')?.hasError('minlength')
+      ? 'Name should be min.5 characters long ' 
+      : this.formGroup.get('password_valid')?.hasError('maxlength')
+      ? 'Name should be max.20 characters long '
       : '';
   }
+
 
 
   onNoClick(): void {
